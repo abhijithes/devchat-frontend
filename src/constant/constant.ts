2f5f1d13f1b720ddc8ae33017f8ccd55d1859ae8
abhijithes <abhijithes266@gmail.com>
2025-09-27 18:21:50 +0530

    added constants

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,3 +1,10 @@
-export const dev_api_url = 'http://localhost:5001'
-export const producttion_api_url = 'https://devchat-backend-kpfa.onrender.com'
-export const api_url = 'https://devchat-backend-kpfa.onrender.com';
+const isProduction = true;
+export const dev_api_url = 'http://localhost:5001/api'
+export const producttion_api_url = 'https://devchat-backend-kpfa.onrender.com/api'
+export const api_url = 'https://devchat-backend-kpfa.onrender.com/api';
+export const current_url = isProduction ? api_url : dev_api_url;
+
+export const endpoints = {
+    register:   `${current_url}/register`,
+    login: `${current_url}/login`
+}
