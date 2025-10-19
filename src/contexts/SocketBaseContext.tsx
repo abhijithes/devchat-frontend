69ef70284bdb545e1ed811747dee5f78530c0c2b
Manuraj tm <manurajtm707@gmail.com>
2025-10-19 16:20:31 +0530

    update:socket hook

--- src/contexts/SocketBaseContext.tsx
+++ src/contexts/SocketBaseContext.tsx
@@ -2,19 +2,20 @@ import { createContext, useContext, type ReactNode } from "react";
 import { io, type Socket } from "socket.io-client";
 import { socket_url } from "../constant/constant";
 
-const SocketBaseContext = createContext(undefined);
+interface SocketContextType {
+  socket: Socket;
+}
+
+const SocketBaseContext = createContext<SocketContextType | undefined>(
+  undefined
+);
+
+const socket: Socket = io(socket_url, {
+  transports: ["websocket", "polling"],
+  autoConnect: true,
+});
 
 const SocketBaseProvider = ({ children }: { children: ReactNode }) => {
-  const socket: Socket = io(socket_url, {});
-
-  socket.on("connection", () => {
-    console.log("Socket connected:", socket.id);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Socket disconnected");
-  });
-
   return (
     <SocketBaseContext.Provider value={{ socket }}>
       {children}
@@ -22,8 +23,11 @@ const SocketBaseProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-const userSocketBaseContext = (): Socket => {
-  return useContext(SocketBaseContext);
+const useSocket = (): Socket => {
+  const context = useContext(SocketBaseContext);
+  if (!context)
+    throw new Error("useSocket must be used within SocketBaseProvider");
+  return context.socket;
 };
 
-export { SocketBaseContext, SocketBaseProvider, userSocketBaseContext };
+export { SocketBaseContext, SocketBaseProvider, useSocket };
---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,35 @@
+import { Outlet } from "react-router-dom";
+import { useSocket } from "../contexts/SocketBaseContext";
+import { useEffect } from "react";
+import type { Socket } from "socket.io-client";
+import { getUserPublicInfo } from "../utils/token";
+
+const AppLayout = () => {
+  const socket: Socket = useSocket();
+  const user = getUserPublicInfo();
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Socket connected with ID:", socket.id);
+    });
+
+    socket.emit("register", user?.id);
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected");
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+    };
+  }, [socket]);
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default AppLayout;
