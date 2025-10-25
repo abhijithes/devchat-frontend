4f1973ea39fe742b5932c48cb7fe4908b35c6e87
Manuraj tm <manurajtm707@gmail.com>
2025-10-25 18:38:22 +0530

    update:image-hieght

--- src/components/view-profile-picture-component/ProfileFullView.tsx
+++ src/components/view-profile-picture-component/ProfileFullView.tsx
@@ -17,7 +17,7 @@ const ProfileFullView = ({
         onClick={(e) => {
           e.stopPropagation();
         }}
-        className=" w-full md:w-md min-h-1/2 h-max bg-white flex flex-col items-center  rounded-md border border-zinc-500 shadow-2xl overflow-hidden"
+        className=" w-full md:w-md min-h-1/2 max-h-[70%] h-max bg-white flex flex-col items-center  rounded-md border border-zinc-500 shadow-2xl overflow-hidden"
       >
         <div className="w-full h-10  flex items-center justify-between px-5 ">
           <h1>View profile</h1>
@@ -32,7 +32,7 @@ const ProfileFullView = ({
         <img
           src={src || Avatar}
           alt="Profile image"
-          className="h-full object-cover"
+          className="h-full object-contain"
         />
       </div>
     </div>
