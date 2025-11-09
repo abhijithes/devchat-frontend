5da2fd603bdc1f0305518a52df51b76d0c6aec04
Manuraj tm <manurajtm707@gmail.com>
2025-11-09 18:03:59 +0530

    table-fixes

--- src/pages/ViewTaskTable/ViewTaskTable.tsx
+++ src/pages/ViewTaskTable/ViewTaskTable.tsx
@@ -4,15 +4,14 @@ import { useParams } from "react-router-dom";
 import TaskTable from "../../components/TaskTable/TaskTable";
 
 const ViewTickets: React.FC = () => {
-    const { projectId } = useParams<{ projectId: string }>();
+  const { projectId } = useParams<{ projectId: string }>();
 
-    if (!projectId) return <div className="p-6 text-red-500 font-semibold">Invalid project ID</div>;
-
+  if (!projectId)
     return (
-        <div className="md:p-6">
-            <TaskTable projectId={projectId} />
-        </div>
+      <div className="p-6 text-red-500 font-semibold">Invalid project ID</div>
     );
+
+  return <TaskTable projectId={projectId} />;
 };
 
 export default ViewTickets;
