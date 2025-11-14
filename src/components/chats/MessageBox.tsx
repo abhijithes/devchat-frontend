a02f683877c226e3b5918191aa8969d6eb5c085c
Manuraj tm <manurajtm707@gmail.com>
2025-11-15 00:00:29 +0530

    user-icons-added

--- src/components/chats/MessageBox.tsx
+++ src/components/chats/MessageBox.tsx
@@ -1,13 +1,16 @@
 import { MessageSquareCode } from "lucide-react";
 import { AiIcon } from "../../constant/icons";
 import type { Message } from "../../constant/messages";
+import UserIcon from "../userIcon/usericon";
+import usersIcons from "../../constant/usersIcons";
 
 const MessageBox = (message: Message) => {
   return (
     <div key={message._id} className="w-full mb-4">
-      <div className="bg-white p-4 rounded hover:bg-blue-50 transition-transform ">
+      <div className="bg-white p-4 flex gap-3 flex-col rounded hover:bg-blue-50 transition-transform ">
+        <UserIcon user={usersIcons[0]} />
         <p className="text-black">{message.content}</p>
-        <span className="text-sm text-gray-500 mt-5 block">
+        <span className="text-sm text-gray-500 block">
           {new Date(message.createdAt).toLocaleString()}
         </span>
       </div>
