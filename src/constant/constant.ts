85fb6ebb9da67db4d6caf1aa0a386ba574569241
Manuraj tm <manurajtm707@gmail.com>
2025-11-30 15:30:11 +0530

    added-style

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -85,5 +85,6 @@ export const endpoints = {
   //Message endpoints
   sendMessage: `${current_url}/message/create`,
   updateMessage: (id) => `${current_url}/message/update/${id}`,
-  deleteMessage: (id) => `${current_url}/message/delete/${id}`
+  deleteMessage: (id) => `${current_url}/message/delete/${id}`,
+  markAsRead: `${current_url}/message/markasread`,
 };
