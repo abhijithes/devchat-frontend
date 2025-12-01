c4aef36fef13be0b40caeb7a0d082eb123078d76
Suhaim <masuhaimofficial@gmail.com>
2025-12-01 07:45:42 +0530

    tick error fix

--- src/pages/TodoApp.tsx
+++ src/pages/TodoApp.tsx
@@ -5,7 +5,7 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 
-import CheckIcon from "@mui/icons-material/Check";
+
 import CloseIcon from "@mui/icons-material/Close";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
