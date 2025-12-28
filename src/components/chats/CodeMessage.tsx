d8a3334ccc0afc40a998e99faa5cdbdd744a438c
abhijithes <abhijithes266@gmail.com>
2025-12-28 21:44:02 +0530

    bugfix: line number alignment in code msg

--- src/components/chats/CodeMessage.tsx
+++ src/components/chats/CodeMessage.tsx
@@ -17,16 +17,16 @@ function CodeMessage({ content }: { content: string }) {
         <div className="max-w-full overflow-x-auto rounded-lg border border-[#30363d] bg-[#0d1117] text-sm">
             <div className="flex">
                 {/* Line numbers */}
-                <div className="select-none px-3 py-2 text-right text-[#6e7681] font-mono">
+                <div className="select-none px-3 py-2 pt-5 text-right text-[#6e7681] font-mono">
                     {lines.map((_, i) => (
-                        <div key={i} className="leading-6">
+                        <div key={i} className="leading-6" style={{ lineHeight: "1.5rem" }}>
                             {i + 1}
                         </div>
                     ))}
                 </div>
 
                 {/* Code */}
-                <pre className="px-3 py-2 leading-6">
+                <pre className="px-3 py-2 leading-6" style={{ lineHeight: "1.5rem" }}>
                     <code ref={codeRef} className="language-javascript font-mono whitespace-pre">
                         {content}
                     </code>
